d6ed6d7f7a0ea18e3d389fdc94e48c7d76e3ac15
Qiyuan Xu <xqyww123@gmail.com>
2023-02-23 15:54:32 +0800

    Working Mode

--- Phi_Semantics/PhiSem_Play_Ground.thy
+++ Phi_Semantics/PhiSem_Play_Ground.thy
@@ -68,7 +68,6 @@ proc XXX:
   thm ZZZ
   ;; $xaa ZZZ XXX YYY
  \<medium_right_bracket> .. .
-
     
 proc YYY:
   input \<open>\<^bold>v\<^bold>a\<^bold>l x \<Ztypecolon> \<nat>[32]\<heavy_comma> \<^bold>v\<^bold>a\<^bold>l y \<Ztypecolon> \<nat>[32]\<close>
