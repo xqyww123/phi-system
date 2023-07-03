607a42f5566b2744dc5b39faade9c2e052d44ce3
Qiyuan Xu <xqyww123@gmail.com>
2023-07-03 19:45:49 +0800

    WIP

--- Phi_System/IDE_CP_Reasoning2.thy
+++ Phi_System/IDE_CP_Reasoning2.thy
@@ -364,11 +364,13 @@ subsection \<open>Special Process for Holes\<close>
 lemma ToA_ex_intro:
   " T \<i>\<m>\<p>\<l>\<i>\<e>\<s> U c \<a>\<n>\<d> P
 \<Longrightarrow> T \<i>\<m>\<p>\<l>\<i>\<e>\<s> ExSet U \<a>\<n>\<d> P"
+  for c :: 'b
   unfolding Imply_def by (simp add: \<phi>expns, metis)
 
 lemma ToA_ex_intro':
   " T \<i>\<m>\<p>\<l>\<i>\<e>\<s> R * \<blangle> U c \<brangle> \<a>\<n>\<d> P
 \<Longrightarrow> T \<i>\<m>\<p>\<l>\<i>\<e>\<s> R * \<blangle> ExSet U \<brangle> \<a>\<n>\<d> P"
+  for c :: 'b
   unfolding Imply_def by (simp add: \<phi>expns, metis)
 
 lemma ToSA_finish': "X \<i>\<m>\<p>\<l>\<i>\<e>\<s> 1 * \<blangle> X \<brangle>"
@@ -530,25 +532,44 @@ lemma [\<phi>reason 3210]:
 
 subsection \<open>Existential\<close>
 
-declare ToA_ex_intro [\<phi>reason 2600]
-declare ToA_ex_intro'[\<phi>reason 2600]
+declare ToA_ex_intro
+declare ToA_ex_intro'
 
-(*
+thm ToA_ex_intro[where c=\<open>id c\<close> for c]
+
 \<phi>reasoner_ML ToA_ex_intro 2600 (\<open>_ \<i>\<m>\<p>\<l>\<i>\<e>\<s> ExSet _ \<a>\<n>\<d> _\<close> | \<open>_ \<i>\<m>\<p>\<l>\<i>\<e>\<s> _ * \<blangle> ExSet _ \<brangle> \<a>\<n>\<d> _\<close>) = \<open>
-fn (ctxt,sequent) =>
+fn (ctxt,sequent) => Seq.make (fn () =>
   let val (_, X'', _) = Phi_Syntax.dest_implication (Thm.major_prem_of sequent)
-      val (has_focus, X) = case X''
-                             of Const(\<^const_name>\<open>ExSet\<close>, _) $ X => (false, X)
-                              | Const(\<^const_name>\<open>times\<close>, _) $ _ $ (Const(\<^const_name>\<open>FOCUS_TAG\<close>, _) $ X)
-                                  => (false, X)
+      fun parse (Const(\<^const_name>\<open>ExSet\<close>, \<^Type>\<open>fun \<^Type>\<open>fun ty _\<close> _\<close>) $ X) = (false, ty, X)
+        | parse (Const(\<^const_name>\<open>times\<close>, _) $ _ $ (
+                    Const(\<^const_name>\<open>FOCUS_TAG\<close>, _) $ (Const(\<^const_name>\<open>ExSet\<close>, \<^Type>\<open>fun \<^Type>\<open>fun ty _\<close> _\<close>) $ X)))
+            = (true, ty, X)
+        | parse X = parse (Envir.beta_eta_contract X)
+      val (has_focus, ty, X) = parse X''
       fun ex_var_is_in_obj_only i (Abs(_,_,X)) = ex_var_is_in_obj_only (i+1) X
-        | ex_var_is_in_obj_only i (Const(\<^const_name>\<open>\<phi>Type\<close>, _) $ _ $ T) = ex_var_is_in_obj_only T
+        | ex_var_is_in_obj_only i (Const(\<^const_name>\<open>\<phi>Type\<close>, _) $ _ $ T) = ex_var_is_in_obj_only i T
+        | ex_var_is_in_obj_only i (Const(\<^const_name>\<open>Subjection\<close>, _) $ X $ _) = ex_var_is_in_obj_only i X
         | ex_var_is_in_obj_only i (Bound j) = j <> i
         | ex_var_is_in_obj_only i (X $ Y) = ex_var_is_in_obj_only i X andalso ex_var_is_in_obj_only i Y
         | ex_var_is_in_obj_only i _ = true
-   in 
-        
-\<close>*)
+      val rule0 = if has_focus
+                  then if ex_var_is_in_obj_only ~1 X
+                  then @{thm' ToA_ex_intro'[where c=\<open>id c\<close> for c]}
+                  else @{thm' ToA_ex_intro'}
+                  else if ex_var_is_in_obj_only ~1 X
+                  then @{thm' ToA_ex_intro[where c=\<open>id c\<close> for c]}
+                  else @{thm' ToA_ex_intro}
+   (* val (ctxt', rule) = 
+        if ex_var_is_in_obj_only ~1 X
+        then let val ([x_name], ctxt') = Variable.add_fixes (Name.invent (Variable.names_of ctxt) "\<x>" 1) ctxt
+                 val x_cterm = Thm.cterm_of ctxt (Free (x_name, ty))
+              in (ctxt',
+                  Thm.instantiate (TVars.make [((("'b",0),\<^sort>\<open>type\<close>), Thm.ctyp_of_cterm x_cterm)],
+                                  Vars.make  [((("c",0),Thm.typ_of_cterm x_cterm), x_cterm)]) rule0)
+             end
+        else (ctxt, rule0)*)
+   in SOME ((ctxt, rule0 RS sequent), Seq.empty)
+  end)\<close>
 
 lemma [\<phi>reason 2800]:
   "(\<And>x.  T x \<i>\<m>\<p>\<l>\<i>\<e>\<s> U \<a>\<n>\<d> P)
