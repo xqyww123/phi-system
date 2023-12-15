8ed43aabe55295fccda67af0fa813f3910ea97c4
Qiyuan Xu <xqyww123@gmail.com>
2023-12-15 14:47:15 +0800

    safe

--- Phi_Examples/PhiEx_Linked_Lst.thy
+++ Phi_Examples/PhiEx_Linked_Lst.thy
@@ -10,7 +10,7 @@ term \<open>0 :: logaddr\<close>
 declare [[\<phi>trace_reasoning = 3]]
 declare [[\<phi>reasoning_step_limit = 200]]
 
- 
+  
     
 \<phi>type_def Linked_Lst' :: \<open>TY \<Rightarrow> (VAL, 'a) \<phi> \<Rightarrow> (fiction, logaddr \<times> 'a list) \<phi>\<close>
   where \<open>((addr, []) \<Ztypecolon> Linked_Lst' TY T) = (Void \<s>\<u>\<b>\<j> addr = 0)\<close>
@@ -18,10 +18,11 @@ declare [[\<phi>reasoning_step_limit = 200]]
         ((next, x) \<Ztypecolon> \<m>\<e>\<m>[addr] \<lbrace> next: \<Pp>\<t>\<r> \<s>\<t>\<r>\<u>\<c>\<t> {next: \<p>\<t>\<r>, data: TY}, data: T \<rbrace>\<heavy_comma>
          (next, ls) \<Ztypecolon> Linked_Lst' TY T
          \<s>\<u>\<b>\<j> next. next \<noteq> 0)\<close>
-   deriving \<open>Abstract_Domain T P \<Longrightarrow> Abstract_Domain (Linked_Lst' TY T) (\<lambda>(a,x). list_all P x \<and> (x = [] \<longleftrightarrow> a = 0)) \<close>
+   deriving (*\<open>Abstract_Domain T P \<Longrightarrow> Abstract_Domain (Linked_Lst' TY T) (\<lambda>(a,x). list_all P x \<and> (x = [] \<longleftrightarrow> a = 0)) \<close>
         and \<open>Identity_Elements\<^sub>E (Linked_Lst' TY T) (\<lambda>(a,l). a = 0 \<and> l = [])\<close>
         and \<open>Identity_Elements\<^sub>I (Linked_Lst' TY T) (\<lambda>(a,l). a = 0 \<or> l = []) (\<lambda>(a,l). a = 0 \<and> l = [])\<close>
-        and \<open>Transformation_Functor (Linked_Lst' TY) (Linked_Lst' TY) T U (set o snd) (\<lambda>_. UNIV) (\<lambda>r (a\<^sub>1,l\<^sub>1) (a\<^sub>2,l\<^sub>2). a\<^sub>1 = a\<^sub>2 \<and> list_all2 r l\<^sub>1 l\<^sub>2) \<close> 
+        and*) \<open> \<c>\<o>\<n>\<d>\<i>\<t>\<i>\<o>\<n> TY' = TY
+          \<Longrightarrow> Transformation_Functor (Linked_Lst' TY) (Linked_Lst' TY') T U (set o snd) (\<lambda>_. UNIV) (\<lambda>r (a\<^sub>1,l\<^sub>1) (a\<^sub>2,l\<^sub>2). a\<^sub>1 = a\<^sub>2 \<and> list_all2 r l\<^sub>1 l\<^sub>2) \<close> 
       (*\<open> Object_Equiv T eq \<Longrightarrow> Object_Equiv (Linked_Lst' TY T) (\<lambda>(a\<^sub>1,l\<^sub>1) (a\<^sub>2,l\<^sub>2). a\<^sub>1 = a\<^sub>2 \<and> list_all2 eq l\<^sub>1 l\<^sub>2) \<close>*)
 
 
