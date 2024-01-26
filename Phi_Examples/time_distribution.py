58f9852975c821b7e677181749dc906d36a3d993
Qiyuan Xu <xqyww123@gmail.com>
2024-01-27 06:57:36 +0800

    final statistis

--- Phi_Examples/time_distribution.py
+++ Phi_Examples/time_distribution.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import matplotlib.ticker as tk
 
+from matplotlib import rc
+
+# Set up for using the desired fonts
+rc('font', family='Libertine', size=15)
+rc('text', usetex=True)
+rc('text.latex', preamble=r'\usepackage{libertine}\usepackage{newtxmath}')
+
 origin = np.genfromtxt("timing.csv", delimiter=",")
 
 pseudocount = 1
@@ -12,9 +19,9 @@ proof_eval = origin[:,1] / 1000.0
 proof_search = origin[:,2]
 total = origin[:,3]
 #total = 
-print (total - (reasoning + proof_eval + proof_search))
+#print (total - (reasoning + proof_eval + proof_search))
 
-proof_eval = proof_eval[proof_eval > 0.01]
+proof_eval = proof_eval[proof_eval > 0]
 proof_search = proof_search[proof_search > 0]
 
 log_reasoning = np.log10(np.maximum(reasoning, 10.0) + pseudocount) 
@@ -22,8 +29,8 @@ log_proof_eval = np.log10(np.maximum(proof_eval, 10.0) + pseudocount)
 log_proof_search = np.log10(np.minimum(np.maximum(proof_search, 10.0), 3000000.0) + pseudocount)
 log_total = np.log10(total + pseudocount)
 
-tick_values = [10.0, 30.0, 100.0, 300.0, 500.0, 1000.0, 3000.0, 5000.0, 10000.0, 30000.0, 100000.0, 300000.0, 1000000]
-tick_names  = ['<0.01    ', '0.03', '0.1', '0.3  ', '  0.5', '1', '3', '5', '10', '30 ', '100', '300', '1000']
+tick_values = [10.0, 30.0, 100.0, 300.0, 1000.0, 3000.0, 10000.0, 30000.0, 100000.0, 300000.0, 1000000]
+tick_names  = ['$\leq 0.01$', '', '0.1', '0.3', '1', '3', '10', '', '', '100 seconds', '']
 log_lower = np.log10(pseudocount + 10.0)
 log_upper = np.log10(pseudocount + 1000000.0)
 
@@ -41,6 +48,8 @@ log_hist2 = normalize_histogram (log_hist2, log_bin_edges2)
 log_hist3 = normalize_histogram (log_hist3, log_bin_edges3)
 log_hist4 = normalize_histogram (log_hist4, log_bin_edges4)
 
+plt.figure(figsize=(4.3, 2.9))
+
 def plot_hist_as_line(hist, bin_edges, label, linestyle, color):
     bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
     plt.plot(bin_centers, hist, label=label, color=color, linestyle=linestyle)
@@ -54,14 +63,19 @@ plot_hist_as_line(log_hist4, log_bin_edges4, 'total', '-', 'black')
 log_tick_positions = [np.log10(tick + pseudocount) for tick in tick_values]
 plt.xticks(log_tick_positions, tick_names)
 
-plt.xlabel('seconds')
+
+plt.yticks([5,10,15,20])
+
+#plt.xlabel('seconds')
 #plt.ylabel('Portion')
 ax = plt.gca()  # Get the current axis
 ax.yaxis.set_major_formatter(tk.PercentFormatter(decimals=0))
 ax.xaxis.set_label_coords(0.955, -0.025)
 ax.xaxis.label.set_bbox(dict(facecolor='white', edgecolor='none', pad=3))
+ax.xaxis.get_majorticklabels()[2].set_horizontalalignment("right")
+#ax.xaxis.get_majorticklabels()[6].set_horizontalalignment("left")
 
-plt.legend()
+plt.legend(framealpha=0.75, labelspacing=0.2, loc='upper center')
 
 plt.grid(linestyle=':')
 
